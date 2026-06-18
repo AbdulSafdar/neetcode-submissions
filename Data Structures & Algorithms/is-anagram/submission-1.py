4acfd8d67e6e71e1b301fd08c3fb2626f56740ce
Abdul Moiz Safdar <abdulsafdar825@gmail.com>
2026-06-17 18:36:36 -0600

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        countMap_s = {}
+        countMap_t = {}
+
+        for l in s:
+            if l in countMap_s:
+                countMap_s[l]+=1
+            else:
+                countMap_s[l] = 1
+        
+        for l in t:
+            if l in countMap_t:
+                countMap_t[l]+=1
+            else:
+                countMap_t[l] = 1
+
+        if countMap_s == countMap_t:
+            return True
+        else:
+            return False
