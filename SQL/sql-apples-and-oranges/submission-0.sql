0a79667f1b91d5a07946df0a134e1e870be4342e
Abdul Moiz Safdar <abdulsafdar825@gmail.com>
2026-07-07 00:31:24 -0600

    Add: sql-apples-and-oranges - submission-0

---
+++ SQL/sql-apples-and-oranges/submission-0.sql
@@ -0,0 +1,8 @@
+-- Write your query below
+SELECT s1.sale_date, s1.sold_num - s2.sold_num AS diff
+FROM sales AS s1
+JOIN sales AS s2
+ON s1.sale_date = s2.sale_date
+WHERE s1.fruit = 'apples' 
+AND s2.fruit = 'oranges'
+ORDER BY s1.sale_date
